e80b824bfda2531c92d2b3546a18d57425d2f80d
avatar42 avatar42_github@m42.biz <avata@Hero.localdomain>
2018-01-04 11:57:33 -0600

    uncomment DefaultJMSConnectionFactory lines

--- src/main/java/org/javaee7/movieplex7/points/ReceivePointsBean.java
+++ src/main/java/org/javaee7/movieplex7/points/ReceivePointsBean.java
@@ -42,10 +42,12 @@ package org.javaee7.movieplex7.points;
 import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.jms.JMSConnectionFactory;
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
 import javax.jms.JMSDestinationDefinition;
@@ -56,40 +58,39 @@ import javax.jms.QueueBrowser;
 /**
  * @author Arun Gupta
  */
-@JMSDestinationDefinition(name = "java:global/jms/pointsQueue",
-        interfaceName = "javax.jms.Queue")
+@JMSDestinationDefinition(name = "java:global/jms/pointsQueue", interfaceName = "javax.jms.Queue")
 @Named
 @RequestScoped
 public class ReceivePointsBean {
 
-    @Inject
-//    @JMSConnectionFactory("java:comp/DefaultJMSConnectionFactory")
-    JMSContext context;
-    
-    @Resource(lookup = "java:global/jms/pointsQueue")
-    Queue pointsQueue;
+	@Inject
+	@JMSConnectionFactory("java:comp/DefaultJMSConnectionFactory")
+	JMSContext context;
 
-    public String receiveMessage() {
-        try (JMSConsumer consumer = context.createConsumer(pointsQueue)) {
-            String message = consumer.receiveBody(String.class);
-            System.out.println("Received message: " + message);
-            return message;
-        }
-    }
+	@Resource(lookup = "java:global/jms/pointsQueue")
+	Queue pointsQueue;
 
-    public int getQueueSize() {
-        int count = 0;
-        try {
-            QueueBrowser browser = context.createBrowser(pointsQueue);
-            Enumeration elems = browser.getEnumeration();
-            while (elems.hasMoreElements()) {
-                elems.nextElement();
-                count++;
-            }
-        } catch (JMSException ex) {
-            Logger.getLogger(ReceivePointsBean.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("Getting queue size: " + count);
-        return count;
-    }
+	public String receiveMessage() {
+		try (JMSConsumer consumer = context.createConsumer(pointsQueue)) {
+			String message = consumer.receiveBody(String.class);
+			System.out.println("Received message: " + message);
+			return message;
+		}
+	}
+
+	public int getQueueSize() {
+		int count = 0;
+		try {
+			QueueBrowser browser = context.createBrowser(pointsQueue);
+			Enumeration elems = browser.getEnumeration();
+			while (elems.hasMoreElements()) {
+				elems.nextElement();
+				count++;
+			}
+		} catch (JMSException ex) {
+			Logger.getLogger(ReceivePointsBean.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		System.out.println("Getting queue size: " + count);
+		return count;
+	}
 }
--- src/main/java/org/javaee7/movieplex7/points/SendPointsBean.java
+++ src/main/java/org/javaee7/movieplex7/points/SendPointsBean.java
@@ -43,6 +43,7 @@ import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.jms.JMSConnectionFactory;
 import javax.jms.JMSContext;
 import javax.jms.Queue;
 import javax.validation.constraints.NotNull;
@@ -55,28 +56,28 @@ import javax.validation.constraints.Pattern;
 @RequestScoped
 public class SendPointsBean {
 
-    @Inject
-//    @JMSConnectionFactory("java:comp/DefaultJMSConnectionFactory")
-    JMSContext context;
-    
-    @NotNull
-    @Pattern(regexp = "^\\d{2},\\d{2}", message = "Message format must be 2 digits, comma, 2 digits, e.g. 12,12")
-    private String message;
+	@Inject
+	@JMSConnectionFactory("java:comp/DefaultJMSConnectionFactory")
+	JMSContext context;
 
-    public String getMessage() {
-        return message;
-    }
+	@NotNull
+	@Pattern(regexp = "^\\d{2},\\d{2}", message = "Message format must be 2 digits, comma, 2 digits, e.g. 12,12")
+	private String message;
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	public String getMessage() {
+		return message;
+	}
 
-    @Resource(lookup = "java:global/jms/pointsQueue")
-    Queue pointsQueue;
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public void sendMessage() {
-        System.out.println("Sending message: " + message);
+	@Resource(lookup = "java:global/jms/pointsQueue")
+	Queue pointsQueue;
 
-        context.createProducer().send(pointsQueue, message);
-    }
+	public void sendMessage() {
+		System.out.println("Sending message: " + message);
+
+		context.createProducer().send(pointsQueue, message);
+	}
 }
